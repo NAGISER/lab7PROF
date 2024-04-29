c774bf908d9904040b994d7e2d35713009614031
NAGISER <pasha.shamanov10@mail.ru>
2024-04-29 10:07:07 +0300

    XML

---
+++ XmlService.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Service.h"
+#include "pugixml-1.13/src/pugixml.hpp"
+using namespace pugi;
+class XmlService:public Service
+{
+public:
+	virtual Weather getWeather(std::string s) override;
+	virtual ~XmlService() {};
+};
+
